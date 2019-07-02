2bf08ae25eaceb36453d70f6fb6c31847a474315
Chinmay Hebbar <4chinmai@gmail.com>
2019-07-02 18:50:27 +0530

    Working Game of Life

---
+++ src/Buttons.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {ButtonToolbar} from 'react-bootstrap';
+
+export default class Buttons extends React.Component {
+    render(){
+        return (
+        <div className="center">
+            <ButtonToolbar>
+                <button className="btn btn-default" onClick={this.props.play}>
+                    Play
+                </button>
+                <button className="btn btn-default" onClick={this.props.pauseButton}>
+                  Pause
+                </button>
+                <button className="btn btn-default" onClick={this.props.clear}>
+                  Clear
+                </button>
+                <button className="btn btn-default" onClick={this.props.seed}>
+                  Seed
+                </button>
+            </ButtonToolbar>
+        </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Grid from './Grid';
+import Buttons from './Buttons';
 
 
 function deepClone(arr) {
@@ -9,7 +10,9 @@ function deepClone(arr) {
 }
 
 class Main extends React.Component {
-    speed = 100;
+    // speed is update time!
+    // speed = 50;
+    speed = 3000;
     rows = 30;
     cols = 50;
     state = {
@@ -48,27 +51,61 @@ class Main extends React.Component {
         var ng = deepClone(g);
         for(let i = 0; i < this.rows; i++){
             for(let j = 0; j < this.cols; j++){
-                let adjacent = 0;
+                var adjacent = 0;
+
+                // adjacent - left and right
+                adjacent += (i === 0)? 0 : g[i - 1][j];
+                adjacent += (i === this.rows - 1)? 0 : g[i + 1][j];
+
+                // adjacent - top and bottom
+                adjacent += (j === 0)? 0 : g[i][j - 1];
+                adjacent += (j === this.cols - 1)? 0 : g[i][j + 1];
+
+                // diagonals
+                adjacent += (j === 0 || i === 0)? 0 : g[i-1][j - 1];
+                adjacent += (i === 0 || j === this.cols - 1)? 0 : g[i - 1][j + 1];
+                adjacent += (i === this.rows - 1 || j === 0)? 0 : g[i + 1][j - 1];
+                adjacent += (i === this.rows - 1 || j === this.rows - 1)? 0 : g[i + 1][j + 1];
+
+                // check rules for life and create/destroy
+                if(g[i][j] && (adjacent < 2 || adjacent > 3)){
+                    ng[i][j] = 0;
+                }
+                if(!(g[i][j]) && adjacent === 3){
+                    ng[i][j] = 1;
+                }
             }
         }
+        this.setState({
+            gridFull: ng,
+            generation: this.state.generation + 1,
+        });
     }
 
     componentDidMount() {
         // Change ratio 
         var seedNo = Math.floor((this.rows*this.cols)/4);
         this.seeder(seedNo);
+        this.playButton();
     }
 
     render() {
         return (
             <div>
-                <h1> Logistic Growth based Game of Life </h1>
+                <h1> Chinmay's Logistic Growth based Game of Life </h1>
+                <Buttons
+                    playButton={this.playButton}
+                    pauseButton={this.pauseButton}
+                    clear={this.clear}
+                    seed={this.seeder}
+                />
                 <Grid 
                     gridFull ={this.state.gridFull}
                     rows={this.rows}
                     cols={this.cols}
                     selectBox={this.selectBox}
                 />
+                <h3>Generation: {this.state.generation}</h3>
             </div>
         );
     }

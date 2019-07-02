0f5e0112e03c705994e9d326cd10843cb4c07719
Chinmay Hebbar <4chinmai@gmail.com>
2019-07-02 16:06:00 +0530

    Create basic grid component

---
+++ src/Grid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Grid.css';
+
+export default class Grid extends React.Component {
+
+    render(){
+        return (
+            <div>
+                Grid
+            </div>
+        );
+    }
+}

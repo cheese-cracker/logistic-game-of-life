39e769ca78d1c3f26e4235c6fb9fd1536d415cc1
Chinmay Hebbar <4chinmai@gmail.com>
2019-07-02 17:30:56 +0530

    Make Grid with Seeder and selection

--- src/Grid.js
+++ src/Grid.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import './Grid.css';
 
+
+class Box extends React.Component {
+    selectBox = () => {
+        this.props.selectBox(this.props.row, this.props.col);
+    }
+    render(){
+        return(
+            <div
+                className={this.props.boxClass}
+                id={this.props.boxId}
+                onClick={this.selectBox}
+            />
+        );
+    }
+}
+
 export default class Grid extends React.Component {
 
     render(){
+        const width = this.props.cols * 16;
+        var rowsArr = [];
+        for ( var i = 0; i < this.props.rows; i++){
+            for ( var j = 0; j < this.props.rows; j++){
+                var boxId = "box_" + i + "_" + j;
+                var boxClass = this.props.gridFull[i][j] ? "box on": "box off";
+                rowsArr.push(
+                    <Box
+                        boxClass={boxClass}
+                        key={boxId}
+                        boxId = {boxId}
+                        row={i}
+                        col={j}
+                        selectBox={this.props.selectBox}
+                    />
+                );
+            }
+        }
+
         return (
-            <div>
-                Grid
+            <div className="grid" style={{width: width}}>
+                {rowsArr}
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -4,17 +4,71 @@ import './index.css';
 import Grid from './Grid';
 
 
+function deepClone(arr) {
+    return JSON.parse(JSON.stringify(arr));
+}
+
 class Main extends React.Component {
+    speed = 100;
+    rows = 30;
+    cols = 50;
     state = {
         generation: 0,
+        gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false)),
     }
 
+    selectBox = (row, col) => {
+        var newGrid = deepClone(this.state.gridFull);
+        newGrid[row][col] = !(newGrid[row][col]);
+        this.setState({
+            gridFull: newGrid
+        });
+    }
+
+    seeder = (num) => {
+        var newGrid = deepClone(this.state.gridFull);
+        for(let n = 0; n < num; n++){
+            var a_i = Math.floor(Math.random()*this.rows);
+            var a_j = Math.floor(Math.random()*this.cols);
+            console.log("Seeded", a_i, a_j);
+            newGrid[a_i][a_j] = 1;
+        }
+        this.setState({
+            gridFull: newGrid
+        });
+    }
+
+    playButton = () => {
+        clearInterval(this.intervalId);
+        this.intervalId = setInterval(this.play, this.speed);
+    }
+
+    play = () => {
+        var g = this.state.gridFull;
+        var ng = deepClone(g);
+        for(let i = 0; i < this.rows; i++){
+            for(let j = 0; j < this.cols; j++){
+                let adjacent = 0;
+            }
+        }
+    }
+
+    componentDidMount() {
+        // Change ratio 
+        var seedNo = Math.floor((this.rows*this.cols)/4);
+        this.seeder(seedNo);
+    }
 
     render() {
         return (
             <div>
                 <h1> Logistic Growth based Game of Life </h1>
-                <Grid />
+                <Grid 
+                    gridFull ={this.state.gridFull}
+                    rows={this.rows}
+                    cols={this.cols}
+                    selectBox={this.selectBox}
+                />
             </div>
         );
     }
